14b5ba349568f09e9351ccb4ce4d92aa07b6c924
rodion <rodion.prosvirnin@bk.ru>
2019-07-03 21:18:54 +0300

    repository calls inside loadData() and closing method onCleared()

--- app/src/main/java/ru/skillbranch/devintensive/architecturecomponentsedu/viewmodel/Main4Activity.java
+++ app/src/main/java/ru/skillbranch/devintensive/architecturecomponentsedu/viewmodel/Main4Activity.java
@@ -1,6 +1,9 @@
 package ru.skillbranch.devintensive.architecturecomponentsedu.viewmodel;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,7 +18,25 @@ public class Main4Activity extends AppCompatActivity {
 
         //getting our viewmodel
         MyViewModel model = ViewModelProviders.of(this).get(MyViewModel.class);
+        LiveData<String> data = model.getData();
+        data.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                // ...
+            }
+        });
         //У метода get, который возвращает нам модель из провайдера, есть еще такой вариант вызова:
         //T get (String key, Class<T> modelClass)
     }
 }
+
+/* EXPLANATION
+- Activity вызывает метод модели getData
+- модель создает MutableLiveData и стартует асинхронный процесс получения данных от репозитория
+- Activity подписывается на полученный от модели LiveData и ждет данные
+- происходит поворот экрана
+- на модели этот поворот никак не сказывается, она спокойно сидит в провайдере и ждет ответ от репозитория
+- Activity пересоздается, получает ту же самую модель от провайдера, получает тот же самый LiveData от модели и подписывается на него и ждет данные
+- репозиторий возвращает данные, модель передает их в MutableLiveData
+- Activity получает данные данные от LiveData
+ */
--- app/src/main/java/ru/skillbranch/devintensive/architecturecomponentsedu/viewmodel/MyViewModel.java
+++ app/src/main/java/ru/skillbranch/devintensive/architecturecomponentsedu/viewmodel/MyViewModel.java
@@ -7,6 +7,7 @@ import android.arch.lifecycle.ViewModel;
 //ViewModel - класс, позволяющий Activity и фрагментам сохранять необходимые им объекты живыми при повороте экрана.
 //Не храните в ViewModel ссылки на Activity, фрагменты, View и пр. Это может привести к утечкам памяти.
 //Модель жива, пока Activity не закроется окончательно.
+//в модели понадобился объект Context, то вы можете наследовать не ViewModel, а AndroidViewModel
 public class MyViewModel extends ViewModel {
 
     MutableLiveData<String> data;
@@ -19,6 +20,21 @@ public class MyViewModel extends ViewModel {
         return data;
     }
 
+    /*
+    ViewModel asks data from the repository:
+
+    LiveData<String> data;
+
+    public LiveData<String> getData() {
+        if (data == null) {
+            data = dataRepository.loadData();
+        }
+        return data;
+    }
+     */
+
+    //В методе loadData происходит асинхронное получение данных из какого-нибудь репозитория.
+    //Как только данные будут получены (в методе onLoad), мы передаем их в MutableLiveData.
     private void loadData() {
         /*dataRepository.loadData(new Callback<String>() {
             @Override
@@ -26,5 +42,12 @@ public class MyViewModel extends ViewModel {
                 data.postValue(s);
             }
         });*/
+    }
+
+    //Когда Activity окончательно закрывается, провайдер удаляет ViewModel, предварительно вызвав его метод onCleared
+    @Override
+    protected void onCleared() {
+        // clean up resources
+
     }
 }
